d87ad1ddf3208bfd8447a45cdb4c71d16f1273d3
Tony Sulfaro <tony116523@gmail.com>
2018-09-05 13:26:26 -0400

    fixed formatting issues

--- Assingments/module03/week01/sulfaroa.inclass.py
+++ Assingments/module03/week01/sulfaroa.inclass.py
@@ -6,19 +6,20 @@ In-Class Exercise
 
 import math
 
+
 def print_wave(num_rows, num_cols):
 
-    outfile = open('./Assingments/module03/week01/sulfaroa.inclass.output.csv', 'w')
+    outfile = open('sulfaroa.inclass.output.csv', 'w')
 
     for x in range(num_rows):
         x_location = math.sin(math.radians(x/num_rows)*360)
 
-        #find x location in columns
+        # find x location in columns
         tmp_location = int(x_location*(num_cols/2)+(num_cols/2))
 
         line = ''
 
-        #generate row with x in place
+        # generate row with x in place
         for y in range(num_cols):
             if y == tmp_location:
                 line+= 'X,'
@@ -30,9 +31,11 @@ def print_wave(num_rows, num_cols):
         outfile.write(line)
 
     outfile.close()
-        
+
+
 def main():
-    print_wave(31,60)
+    print_wave(31, 60)
+
 
 if __name__ == "__main__":
     main()
