9a1e60a23c41627d8d97bbb58aa8ffd643bfc5e4
Tony Sulfaro <tony116523@gmail.com>
2018-09-10 11:48:12 -0400

    added docstring

--- Assingments/module03/week02/sulfaroa.inclass.py
+++ Assingments/module03/week02/sulfaroa.inclass.py
@@ -1,4 +1,9 @@
 # use the imgur API and get the most popular cat picture
+"""
+sulfaroa
+9/10/18
+In-class activity
+"""
 
 from imgurpython import ImgurClient
 
@@ -6,11 +11,6 @@ client_id = '82d55c3dec952aa'
 client_secret = '3e2e0865723e9a6b94100702980b58ca95a0fd25'
 
 client = ImgurClient(client_id, client_secret)
-
-# Example request
-#items = client.gallery(section='top', sort='time', page=0, window='week', show_viral=True)
-#for item in items:
-    #print(item.link)
 
 tag_collection = client.gallery_search('cat', advanced=None, sort='top', page=0)
 
