a9c6d99caf8f6917af07617d7485aab3f078b173
Tony Sulfaro <tony116523@gmail.com>
2018-09-11 21:56:09 -0400

    modified in class to save to directory

--- Assingments/module03/week02/sulfaroa.inclass.py
+++ Assingments/module03/week02/sulfaroa.inclass.py
@@ -6,12 +6,20 @@ In-class activity
 """
 
 from imgurpython import ImgurClient
+import requests
 
 client_id = '82d55c3dec952aa'
 client_secret = '3e2e0865723e9a6b94100702980b58ca95a0fd25'
 
 client = ImgurClient(client_id, client_secret)
 
+# get images with cat
 tag_collection = client.gallery_search('cat', advanced=None, sort='top', page=0)
 
-print(tag_collection[1].link)
+# slice out first image
+image_url = tag_collection[0].link
+
+# get image then write it
+img_data = requests.get(image_url).content
+with open('cat_picture.jpg', 'wb') as handler:
+    handler.write(image_url)
